ba0d821b891bdefaf81643290d0586ab83028f71
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-15 23:24:04 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,22 @@
+class TicTacToe
+
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " "," ", " "]
+  end
+
+  def display_board
+    @board
+  end
+
+  def input_to_index(choice)
+    num = choice.to_i
+    puts num
+    if num > 0 && num < 9
+    num -= 1
+    end
+      @board[num]
+  end
+
+end #end of class
